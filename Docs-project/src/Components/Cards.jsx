d4ad997c6e6509662c4a12d7af610f08201c2dd7
sonustar <91054530+sonustar@users.noreply.github.com>
2023-12-21 18:43:47 +0000

    all done

--- Docs-project/src/Components/Cards.jsx
+++ Docs-project/src/Components/Cards.jsx
@@ -1,11 +1,32 @@
 
-
-function Cards(){
+import { FaRegFileAlt } from "react-icons/fa";
+import { FaArrowAltCircleDown } from "react-icons/fa";
+import { GiCrossMark } from "react-icons/gi";
+function Cards({x}){
 
 return (
-    <div className="w-60 h-72 rounded-[20px] bg-zinc-600">
+    <div className=" relative w-60 h-80 rounded-[30px] bg-zinc-600 px-5 py-10 text-white overflow-hidden ">
+    <FaRegFileAlt/>
+   <p className="text-sm leading-tight font-semibold  mt-5 ">{x.desc}{x.name} </p>   
+      <div className=" footer absolute bottom   w-full h-10 left-0"> 
         
-    </div>
+          
+          <div className="flex items-center justify-between ml-4 mr-5 mb-10  py-5 ">
+                <h5> </h5>
+                <span className="w-6  h-4 bg-zinc-600 rounded-full flex items-center justify center">
+                {x.value ?  <FaArrowAltCircleDown   size={".9em"} color='#fff'/> : <GiCrossMark />  }
+                </span>
+         </div>
+         
+         <div className="tag bg-green-600 h-10 w-full bottom text-white"> 
+
+          <h3 className=" flex justify-center bottom-0 p-3">Download</h3>
+         
+         </div>
+    
+       </div>
+    
+     </div>
 );
 
 }
--- Docs-project/src/Components/Foreground.jsx
+++ Docs-project/src/Components/Foreground.jsx
@@ -3,10 +3,27 @@ import { FaRegFileAlt } from "react-icons/fa";
 
 function Foreground(){
 
+  const arr =[
+   {
+      name : "Sonu",
+      desc:"hii i study in Jadavpur University , Persuing : B.tech .",
+      size:"50MB",
+      value:false,
+   } 
+   
+  ];
+
+
   return (
     
     <div className='fixed top-0 left-0 z-[3] h-full w-full'>
-    <Cards/>  
+
+    // use map 
+    {arr.map((item)=>(
+    <Cards x={item} />   
+      
+    ))}
+
     </div>
 
   );
