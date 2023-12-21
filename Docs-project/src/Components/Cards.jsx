d9883b8fed688c8078a540787d921b6ec0df5964
sonustar <91054530+sonustar@users.noreply.github.com>
2023-12-21 16:52:30 +0000

    All changed

---
+++ Docs-project/src/Components/Cards.jsx
@@ -0,0 +1,13 @@
+
+
+function Cards(){
+
+return (
+    <div className="w-60 h-72 rounded-[20px] bg-zinc-600">
+        
+    </div>
+);
+
+}
+
+export default Cards;
---
+++ Docs-project/src/Components/Foreground.jsx
@@ -0,0 +1,17 @@
+import Cards from "./Cards"
+import { FaRegFileAlt } from "react-icons/fa";
+
+function Foreground(){
+
+  return (
+    
+    <div className='fixed top-0 left-0 z-[3] h-full w-full'>
+    <Cards/>  
+    </div>
+
+  );
+
+
+}
+
+export default Foreground;
